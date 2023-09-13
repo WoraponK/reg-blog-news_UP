3eaf2487fab79ce741998ddee987cdf5b6d461ff
WoraponK <worapon.klabsri@gmail.com>
2023-09-13 14:59:44 +0700

    create scroll to top button

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 const secret = document.querySelector(".secret-image")
 const body = document.body
-window.onscroll = function() {myFunction()};
-
-const navbar = document.getElementById("navbar");
+const navbar = document.getElementById("navbar")
 const sticky = navbar.offsetTop;
+const scrollBtn = document.getElementById("scrollToTopBtn")
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
+window.onscroll = () => {stickyNavbar(); scrollToTop();}
+
+const stickyNavbar = () => {
+  if (window.scrollY >= sticky) {
     navbar.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    navbar.classList.remove("sticky")
   }
 }
 
 secret.addEventListener("click", () => {
-    
     secret.classList.toggle("open")
-
-
 })
+
+const scrollToTop = () => {
+    if (body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        scrollBtn.style.display = "block"
+    } else {
+        scrollBtn.style.display = "none"
+    }
+}
+
+const topFunction = () => {
+    body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+}
